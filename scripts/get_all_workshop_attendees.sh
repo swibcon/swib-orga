bc37b412554725abf17377fc4d246a816c432635
Phu Tu <phu.tu@hbz-nrw.de>
2024-11-05 16:16:25 +0100

    Print csv amd markdown for duplicates only

--- scripts/get_all_workshop_attendees.sh
+++ scripts/get_all_workshop_attendees.sh
@@ -33,5 +33,11 @@ done
 # print output to file
 printf '%s\n' "${all_workshop_attendees[@]}" | sort -k1 > all_workshop_attendees.csv
 
+# print duplicates only
+awk -F' ' '$1 in first{print first[$1] $0; first[$1]=""; next} {first[$1]=$0 ORS}' all_workshop_attendees.csv > multiple_workshops_attendees.csv
+
+# print markdown including links
+awk '{$1 = "["$1"](https://forum.swib.org/u/"$1")"; $3 = "["$3"](https://forum.swib.org/t/"$3")";  print}' multiple_workshops_attendees.csv > multiple_workshops_attendees.md
+
 echo "$(date) : Finished!"
 
